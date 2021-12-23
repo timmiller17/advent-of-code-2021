231fc49404decbca5e16bc6df78d2d82155b84d7
Tim Miller <tim.miller@e-gineering>
2021-12-23 11:19:54 -0500

    day 14 part 1

---
+++ src/Day14.kt
@@ -0,0 +1,43 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        var polymerTemplate = input[0].chunked(1).toMutableList()
+        val pairInsertionRules = input.dropWhile { it.contains("->").not() }
+
+        val insertionRuleMap = mutableMapOf<String, String>()
+        for (rule in pairInsertionRules) {
+            insertionRuleMap += rule.take(2) to rule.last().toString()
+        }
+
+        repeat(10) {
+            val valuesToInsert = polymerTemplate.zipWithNext { a, b ->
+                insertionRuleMap[arrayListOf(
+                    a,
+                    b
+                ).joinToString("")]
+            }
+
+            val result = polymerTemplate.zip(valuesToInsert).flatMap { listOf(it.first, it.second!!) }.toMutableList()
+            result += polymerTemplate.last().toString()
+
+            polymerTemplate.clear()
+            polymerTemplate.addAll(result)
+        }
+
+        val temp = polymerTemplate.groupBy { it }.values
+
+        return temp.maxOf { it.size } - temp.minOf { it.size }
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day14_test")
+    check(part1(testInput) == 1588)
+//    check(part2(testInput) == 2188189693529)
+
+    val input = readInput("Day14")
+    println("part1 ${part1(input)}")
+    println("part2 ${part2(input)}")
+}
