525571d37406c33e94c27e951597b4a9e72d2147
Tim Miller <tim.miller@e-gineering>
2021-12-22 22:53:30 -0500

    day 13 part 1

---
+++ src/Day13.kt
@@ -0,0 +1,45 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        val points = input.filter { it.contains(",") }.map { it.split(",") }.map { Pair(it[0].toInt(), it[1].toInt()) }
+        val foldInstructions = input.filter { it.contains("=") }
+            .map {  it.split("=") }
+            .map { FoldInstruction(it[0].last().toString(), it[1].toInt()) }
+
+        val foldedPoints = mutableSetOf<Pair<Int, Int>>()
+        for (point in points) {
+            val direction = foldInstructions[0].direction
+            val position = foldInstructions[0].position
+            if (direction == "y") {
+                if (point.second < position) {
+                    foldedPoints += point
+                } else {
+                    val newY = position - (point.second - position)
+                    foldedPoints += Pair(point.first, newY)
+                }
+            } else {
+                if (point.first > position) {
+                    foldedPoints += point
+                } else {
+                    val newX = position + (position - point.first)
+                    foldedPoints += Pair(newX, point.second)
+                }
+            }
+        }
+        return foldedPoints.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day13_test")
+    check(part1(testInput) == 17)
+//    check(part2(testInput) == 5)
+
+    val input = readInput("Day13")
+    println("part1 ${part1(input)}")
+    println("part2 ${part2(input)}")
+}
+
+data class FoldInstruction(val direction: String, val position: Int)
