4de9dde6fb09f1396701d44fb6aaa9b738644a67
Tim Miller <tim.miller@e-gineering>
2022-01-05 20:13:38 -0500

    day 15 part 1

--- src/Day15.kt
+++ src/Day15.kt
@@ -3,6 +3,7 @@ import java.util.*
 fun main() {
     /*
 
+    For Part 0:
     Assuming lowest risk path will only have down and right moves, use algorithm at this site
     https://www.geeksforgeeks.org/print-all-possible-paths-from-top-left-to-bottom-right-of-a-mxn-matrix/
     to find all paths and then choose path of least risk.
@@ -11,6 +12,10 @@ fun main() {
     if I'm lucky, part1.
 
     UPDATE: Works for my extra small part0 and for the part 1 sample, but too slow for part1 real data.
+    
+    Next Update: Implemented Dijkstra algorithm and now working for part1!!!! Thanks Wikipedia!
+    https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+
      */
 
     fun part0(input: List<String>): Int {
@@ -24,13 +29,56 @@ fun main() {
     }
 
     fun part1(input: List<String>): Int {
-        val cave = input.map { it.chunked(1).map { it.toInt() } }
+        val cave = input.map { it.chunked(1).map { it.toInt() }.toMutableList() }.toMutableList()
 
-        val paths = mutableListOf<List<Int>>()
+        val distances = mutableListOf<MutableList<Int>>()
+        val previousNodes = mutableListOf<MutableList<Pair<Int, Int>?>>()
 
-        findAllPaths(cave, paths)
+        for (list in cave) {
+            distances += mutableListOf(list.map { Int.MAX_VALUE }.toMutableList())
+            previousNodes += mutableListOf(list.map { null }.toMutableList())
+        }
+        distances[0][0] = 0
 
-        return paths.minOf { it.sum() } - cave[0][0]
+        val unvisitedSet = mutableSetOf<Node>()
+
+        for (i in cave.indices) {
+            for (j in cave[0].indices) {
+                unvisitedSet += if (i == 0 && j == 0) {
+                    Node(Pair(i,j), 0)
+                } else {
+                    Node(Pair(i,j))
+                }
+            }
+        }
+
+        while (unvisitedSet.isNotEmpty()) {
+            val currentNode = unvisitedSet.minByOrNull { it.distance }!!
+            unvisitedSet.remove(currentNode)
+
+            val location = currentNode.location
+
+            val neighbors = getNeighbors(location, cave.size, cave[0].size)  // get potential neighbors
+            neighbors.removeAll { unvisitedSet.map { node -> node.location }.contains(it).not() } // remove neighbors already visited
+
+            for (neighbor in neighbors) {
+                val distance = distances[location.first][location.second] + getDistance(location, neighbor, cave)
+
+                if (distance < distances[neighbor.first][neighbor.second]) {
+                    distances[neighbor.first][neighbor.second] = distance
+                    previousNodes[neighbor.first][neighbor.second] = location
+                }
+            }
+        }
+
+        val lowestRiskPath = mutableListOf(Pair(cave.size - 1, cave[0].size - 1))
+        var previousNode = previousNodes[cave.size - 1][cave[0].size - 1]
+        while (previousNode != Pair(0, 0)) {
+            lowestRiskPath += previousNode!!
+            previousNode = previousNodes[previousNode!!.first][previousNode.second]
+        }
+
+        return lowestRiskPath.sumOf { cave[it.first][it.second] }
     }
 
     fun part2(input: List<String>): Int {
@@ -45,16 +93,61 @@ fun main() {
 //    check(part2(testInput) == 5)
 
     val input = readInput("Day15")
-//    println("part1 ${part1(input)}")
+    println("part1 ${part1(input)}")
 //    println("part2 ${part2(input)}")
 }
+
+data class Node(
+    val location: Pair<Int, Int>,
+    val distance:Int = Int.MAX_VALUE,
+)
+
+fun getDistance(location: Pair<Int, Int>, neighbor: Pair<Int, Int>, cave: List<List<Int>>): Int {
+    return cave[neighbor.first][neighbor.second]  // Chiton risk is equivalent to distance in adapting Dijkstra's algorithm to this problem space
+}
+
+fun getNeighbors(location: Pair<Int, Int>, caveRows: Int, caveColumns: Int): MutableList<Pair<Int, Int>> {
+    val neighbors = mutableListOf<Pair<Int, Int>>()
+
+    if (location.hasNorthNeighbor()) {
+        neighbors += Pair(location.first - 1, location.second)
+    }
+    if (location.hasWestNeighbor()) {
+        neighbors += Pair(location.first, location.second - 1)
+    }
+    if (location.hasSouthNeighbor(caveRows)) {
+        neighbors += Pair(location.first + 1, location.second)
+    }
+    if (location.hasEastNeighbor(caveColumns)) {
+        neighbors += Pair(location.first, location.second + 1)
+    }
+
+    return neighbors
+}
+
+fun Pair<Int, Int>.hasNorthNeighbor(): Boolean {
+    return this.first > 0
+}
+
+fun Pair<Int, Int>.hasWestNeighbor(): Boolean {
+    return this.second > 0
+}
+
+fun Pair<Int, Int>.hasSouthNeighbor(caveRows: Int): Boolean {
+    return this.first < caveRows
+}
+
+fun Pair<Int, Int>.hasEastNeighbor(caveColumns: Int): Boolean {
+    return this.second < caveColumns
+}
+
+
 
 data class Location(
     val path: List<Int>,
     val i: Int,
     val j: Int,
 )
-
 
 fun findAllPaths(cave: List<List<Int>>, paths: MutableList<List<Int>>) {
     val n = cave.size
